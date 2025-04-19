5e0105cff777eaef1e4388d5a5ff98c8b64fa6ee
Kokhulash <kokhulash736@gmail.com>
2025-04-20 00:47:08 +0530

    added notification functionality

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,36 +1,51 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
 
-import { IoSettingsOutline } from "react-icons/io5";
-import { FaUser } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa6";
+import {IoSettingsOutline} from "react-icons/io5";
+import {FaUser} from "react-icons/fa";
+import {FaHeart} from "react-icons/fa6";
+
+import {useQuery, useMutation, useQueryClient} from "@tanstack/react-query";
+import {toast} from "react-hot-toast";
 
 const NotificationPage = () => {
-    const isLoading = false;
-    const notifications = [
-        {
-            _id: "1",
-            from: {
-                _id: "1",
-                username: "johndoe",
-                profileImg: "/avatars/boy2.png",
-            },
-            type: "follow",
+    const queryClient = useQueryClient();
+    const {data: notifications, isLoading} = useQuery({
+        queryKey: ["notifications"],
+        queryFn: async () => {
+            try {
+                const res = await fetch("/api/notifications");
+                const data = await res.json();
+                if (!res.ok) throw new Error(data.error || "Something went wrong");
+                return data;
+            } catch (error) {
+                throw new Error(error);
+            }
         },
-        {
-            _id: "2",
-            from: {
-                _id: "2",
-                username: "janedoe",
-                profileImg: "/avatars/girl1.png",
-            },
-            type: "like",
-        },
-    ];
+    });
 
-    const deleteNotifications = () => {
-        alert("All notifications deleted");
-    };
+    const {mutate: deleteNotifications} = useMutation({
+        mutationFn: async () => {
+            try {
+                const res = await fetch("/api/notifications", {
+                    method: "DELETE",
+                });
+                const data = await res.json();
+
+                if (!res.ok) throw new Error(data.error || "Something went wrong");
+                return data;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+        onSuccess: () => {
+            toast.success("Notifications deleted successfully");
+            queryClient.invalidateQueries({queryKey: ["notifications"]});
+        },
+        onError: (error) => {
+            toast.error(error.message);
+        },
+    });
 
     return (
         <>
@@ -39,7 +54,7 @@ const NotificationPage = () => {
                     <p className='font-bold'>Notifications</p>
                     <div className='dropdown '>
                         <div tabIndex={0} role='button' className='m-1'>
-                            <IoSettingsOutline className='w-4' />
+                            <IoSettingsOutline className='w-4'/>
                         </div>
                         <ul
                             tabIndex={0}
@@ -53,19 +68,19 @@ const NotificationPage = () => {
                 </div>
                 {isLoading && (
                     <div className='flex justify-center h-full items-center'>
-                        <LoadingSpinner size='lg' />
+                        <LoadingSpinner size='lg'/>
                     </div>
                 )}
                 {notifications?.length === 0 && <div className='text-center p-4 font-bold'>No notifications 🤔</div>}
                 {notifications?.map((notification) => (
                     <div className='border-b border-gray-700' key={notification._id}>
                         <div className='flex gap-2 p-4'>
-                            {notification.type === "follow" && <FaUser className='w-7 h-7 text-primary' />}
-                            {notification.type === "like" && <FaHeart className='w-7 h-7 text-red-500' />}
+                            {notification.type === "follow" && <FaUser className='w-7 h-7 text-primary'/>}
+                            {notification.type === "like" && <FaHeart className='w-7 h-7 text-red-500'/>}
                             <Link to={`/profile/${notification.from.username}`}>
                                 <div className='avatar'>
                                     <div className='w-8 rounded-full'>
-                                        <img src={notification.from.profileImg || "/avatar-placeholder.png"} />
+                                        <img src={notification.from.profileImg || "/avatar-placeholder.png"}/>
                                     </div>
                                 </div>
                                 <div className='flex gap-1'>
